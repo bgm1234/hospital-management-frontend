c022876fdaa45b5af420ff96ede1b685ea26e840
bgm1234 <bgm-asln@hotmail.com>
2024-05-18 01:31:13 +0300

    Kullanıcı kayıt ve giriş ekranları yapıldı. Multilanguage özelliği eklendi.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { AppBar, Toolbar, Button } from "@mui/material";
-import { Routes, Route , Link} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import SignUp from "./pages/sign-up";
 import SignIn from "./pages/sign-in";
@@ -13,14 +13,12 @@ import PatientList from "./pages/patient-list";
 import Appointments from "./pages/appointments";
 
 function App() {
-  
   return (
     <div>
       <div>
         <AppBar position="static">
           <Toolbar>
-            <Button
-            component={Link} to={"/"}>
+            <Button component={Link} to={"/"}>
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXd13FJJ9NvzUpsCE8BU1HP0GQnf_Pvgc45w&usqp=CAU"
                 style={{ width: "60px", height: "auto" }}
@@ -28,21 +26,28 @@ function App() {
             </Button>
             <Button color="inherit"></Button>
             <div style={{ flexGrow: 1 }} />
-            <Button color="inherit" component={Link} to={"/sign-in"}>Giriş Yap</Button>
-            <Button color="inherit" component={Link} to={"/sign-up"}>Kayıt Ol</Button>
+            <Button style={{ textTransform: "none" }} color="inherit" component={Link} to={"/sign-in"}>
+              Giriş Yap
+            </Button>
+            <Button style={{ textTransform: "none" }} color="inherit" component={Link} to={"/sign-up"}>
+              Kayıt Ol
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
       <div>
         <Routes>
-          <Route path="/" element={<Home />} >
-            <Route path="/doctors" element={<Doctors/>}></Route>
-            <Route path="/healty-services" element={<HealtyServices/>}></Route>
-            <Route path="/my-patients" element={<MyPatient/>}></Route>
-            <Route path="/my-appointments" element={<MyAppointments/>}></Route>
-            <Route path="/patient-registration" element={<PatientRegistration/>}></Route>
-            <Route path="/patient-list" element={<PatientList/>}></Route>
-            <Route path="/appointments" element={<Appointments/>}></Route>
+          <Route path="/" element={<Home />}>
+            <Route path="/doctors" element={<Doctors />}></Route>
+            <Route path="/healty-services" element={<HealtyServices />}></Route>
+            <Route path="/my-patients" element={<MyPatient />}></Route>
+            <Route path="/my-appointments" element={<MyAppointments />}></Route>
+            <Route
+              path="/patient-registration"
+              element={<PatientRegistration />}
+            ></Route>
+            <Route path="/patient-list" element={<PatientList />}></Route>
+            <Route path="/appointments" element={<Appointments />}></Route>
           </Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
